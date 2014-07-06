305612738c522994e7a24394f4c1610e30225e9c
Goober5000 <Goober5000@387891d4-d844-0410-90c0-e4c51a9137d3>
2014-07-06 04:40:50 +0000

    bump version
    git-svn-id: svn://svn.icculus.org/fs2open/trunk/fsoinstaller@10891 387891d4-d844-0410-90c0-e4c51a9137d3

--- src/com/fsoinstaller/main/FreeSpaceOpenInstaller.java
+++ src/com/fsoinstaller/main/FreeSpaceOpenInstaller.java
@@ -70,7 +70,7 @@ public class FreeSpaceOpenInstaller
 	/**
 	 * Version of the Installer.
 	 */
-	public static final String INSTALLER_VERSION = "2.1.7";
+	public static final String INSTALLER_VERSION = "2.1.8";
 	
 	/**
 	 * URL of the directories where version.txt and filenames.txt reside.
