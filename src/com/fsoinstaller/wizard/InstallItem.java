ef2296478afd145faabde61649940f4864d38468
asarium <asarium@gmail.com>
2017-08-13 00:33:32 +0200

    Add support for generating Windows executables with Launch4j
    This adds an Ant task which converts the compiled installer jar to a
    native executable by wrapping the jar in that executable. This should
    reduce user errors where Java is not associated correctly with the .jar
    extension and it also displays a nice message if Java could not be
    found.
    
    I needed to change a few lines in the source since the code did not
    compile with the options present in build.xml.

--- src/com/fsoinstaller/wizard/InstallItem.java
+++ src/com/fsoinstaller/wizard/InstallItem.java
@@ -1146,7 +1146,7 @@ public class InstallItem extends JPanel
 		modLogger.info("Patching " + triple.getPrePatch().getFilename());
 		
 		// see if the file exists
-		File prePatchFile = IOUtils.getFileIgnoreCase(modFolder, triple.getPrePatch().getFilename());
+		final File prePatchFile = IOUtils.getFileIgnoreCase(modFolder, triple.getPrePatch().getFilename());
 		if (prePatchFile == null)
 		{
 			modLogger.info("File does not exist; cannot patch it");
@@ -1213,7 +1213,6 @@ public class InstallItem extends JPanel
 		Patch patch = new Patch();
 		patch.addProgressListener(new ProgressListener()
 		{
-			@Override
 			public void progressMade(ProgressEvent event)
 			{
 				// in the same way we report progress on downloaded files, do so on patched files 
