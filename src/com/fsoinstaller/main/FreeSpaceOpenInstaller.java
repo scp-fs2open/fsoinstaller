55ae9ef817e194017af34869eb97631f27385422
Goober5000 <ipw47@mailbox.org>
2019-11-07 13:03:22 -0500

    update to 2.3.3

--- src/com/fsoinstaller/main/FreeSpaceOpenInstaller.java
+++ src/com/fsoinstaller/main/FreeSpaceOpenInstaller.java
@@ -78,7 +78,7 @@ public class FreeSpaceOpenInstaller
 	/**
 	 * Version of the Installer.
 	 */
-	public static final String INSTALLER_VERSION = "2.3.2";
+	public static final String INSTALLER_VERSION = "2.3.3";
 	
 	/**
 	 * URL of the directories where version.txt and filenames.txt reside.
