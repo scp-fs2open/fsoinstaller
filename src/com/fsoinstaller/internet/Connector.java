e2c3bb567a9c310d904890b262e1f7d5411842b8
Goober5000 <Goober5000@users.noreply.github.com>
2023-02-25 13:28:58 -0500

    Update src/com/fsoinstaller/internet/Connector.java

--- src/com/fsoinstaller/internet/Connector.java
+++ src/com/fsoinstaller/internet/Connector.java
@@ -235,7 +235,9 @@ public class Connector
 		
 		// send a fake user agent to prevent 403 Forbidden errors on certain servers
 		conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/28.0");
-		conn.setRequestProperty("Accept", "*/*"); // workaround https://bugs.openjdk.org/browse/JDK-8163921
+
+		// work around https://bugs.openjdk.org/browse/JDK-8163921
+		conn.setRequestProperty("Accept", "*/*");
 		
 		return conn;
 	}
