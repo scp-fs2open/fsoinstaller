2a11f62b8a5dabd08df9784eb57f600f2b185e6a
Goober5000 <ipw47@mailbox.org>
2023-03-17 22:45:17 -0400

    bump version

--- src/com/fsoinstaller/main/FreeSpaceOpenInstaller.java
+++ src/com/fsoinstaller/main/FreeSpaceOpenInstaller.java
@@ -78,7 +78,7 @@ public class FreeSpaceOpenInstaller
 	/**
 	 * Version of the Installer.
 	 */
-	public static final String INSTALLER_VERSION = "2.3.4";
+	public static final String INSTALLER_VERSION = "2.3.5";
 	
 	/**
 	 * URL of the directories where version.txt and filenames.txt reside.
